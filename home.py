288e93bdb514aa2db9825a0e738d59f48a7ed4b5
sarathkumar <74712315+SARATHKUMAR-BK@users.noreply.github.com>
2024-10-05 18:39:46 +0000

    Updated the files

--- home.py
+++ home.py
@@ -2,9 +2,11 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+gameList = ["numbers" , "alphabets" , "alphanumeric"]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', gameList=gameList)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
