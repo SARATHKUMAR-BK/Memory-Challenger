751425e3c0a8899a1c2829fb9427aed3d4dc3710
sarathkumar <74712315+SARATHKUMAR-BK@users.noreply.github.com>
2024-10-06 13:39:45 +0000

    New pages html added

--- home.py
+++ home.py
@@ -2,10 +2,22 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-gameList = ["numbers" , "alphabets" , "alphanumeric"]
+gameList = ["number" , "alphabet" , "alphanumeric"]
 
 @app.route("/")
-def hello_world():
+def homepage():
+  return render_template('home.html', gameList=gameList)
+
+@app.route("/<gameType>")
+def play_with_numbers(gameType):
+  return render_template('play.html', gameType=gameType)
+
+@app.route("/alphabets")
+def play_with_alphabets():
+  return render_template('home.html', gameList=gameList)
+
+@app.route("/alphanumeric")
+def play_with_alphanumeric():
   return render_template('home.html', gameList=gameList)
 
 if __name__ == "__main__":
