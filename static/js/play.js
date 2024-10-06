765242b3d995e9536f175bd9f010aab34e9862c6
sarathkumar <74712315+SARATHKUMAR-BK@users.noreply.github.com>
2024-10-06 15:27:34 +0000

    new

--- static/js/play.js
+++ static/js/play.js
@@ -6,7 +6,17 @@ let difficultyLevel = 1;
 function startGame() {
     // Increase the number range based on the difficulty level
     const maxNumber = 1000 * difficultyLevel; // Increase max number based on difficulty
-    currentNumber = Math.floor(Math.random() * 9000) + maxNumber; // Generate a random number
+    var gameType = document.getElementById("submit").getAttribute("gameType");
+
+    if (gameType === "number")
+    {
+        currentNumber = Math.floor(Math.random() * 9000) + maxNumber;
+    }
+    else
+    {
+        
+    }
+     // Generate a random number
     document.getElementById('challenge-number').textContent = currentNumber; // Show number
     setTimeout(() => {
         document.getElementById('challenge-number').textContent = '';
@@ -33,14 +43,16 @@ function checkAnswer() {
 
     document.getElementById('recall-input').value = '';
 
+    resultElement.style.display = "block";
+    document.getElementById("recall-input").disabled = true;
+    document.getElementById("submit").disabled = true;
+
     if (correctCount >= 1) {
         alert('You reached 3 correct answers! Increasing difficulty.');
         correctCount = 0; // Reset correct answer count
         difficultyLevel *= 10; // Increase difficulty level
     }
-    resultElement.style.display = "block";
-    document.getElementById("recall-input").disabled = true;
-    document.getElementById("submit").disabled = true;
+    
     // Start the game again
     startGame();
 
