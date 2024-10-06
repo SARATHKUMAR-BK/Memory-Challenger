58aa67957e267e3a320227948ae6903433f16fa4
sarathkumar <74712315+SARATHKUMAR-BK@users.noreply.github.com>
2024-10-06 17:30:36 +0000

    New game type

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-gameList = ["number" , "alphabet" , "alphanumeric"]
+gameList = ["number" , "alphabet" , "alphanumeric" , "special character"]
 
 @app.route("/")
 def homepage():
